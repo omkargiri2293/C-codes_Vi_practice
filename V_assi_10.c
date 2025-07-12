e0e6c4c24d3f4961fcb1b3ef1356ae3575a53965
omkargiri2293 <omkargiri2293@gmail.com>
2025-07-12 23:59:26 +0530

    6

---
+++ V_assi_10.c
@@ -0,0 +1,35 @@
+/*
+write c program to count the total number of duplicate elements in an array
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+    int n, count = 0;
+
+    printf("enter no of elements:");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("enter elements of array:");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count == ;
+                break;
+            }
+        }
+    }
+    return 0;
+}
