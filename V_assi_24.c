8eb4a837112d496e5526402d68117b52a360fff0
omkargiri2293 <omkargiri2293@gmail.com>
2025-07-25 22:04:12 +0530

    13

--- V_assi_24.c
+++ V_assi_24.c
@@ -9,20 +9,20 @@ void copyString(char *source, char *destination)
 
     while (*source != '\0')
     {
-        *source = *destination;
-        *source++;
-        *destination++;
+        *destination = *source;
+        source++;
+        destination++;
     }
     *destination = '\0';
 }
 int main()
 {
-    int str1[100], str2[100];
+    char str1[100], str2[100];
 
     printf("enter str1 : ");
     scanf("%s", str1);
 
     copyString(str1, str2);
 
-    printf("str2 : %s", str2);
+    printf("Copied string: %s\n", str2);
 }
--- V_assi_26.c
+++ V_assi_26.c
@@ -1,11 +1,47 @@
 /*
+Implement a function to find the frequency of characters.
 
+Example:
+
+Input the string: This is a test string
+
+Input the character to find frequency: i
+
+Expected Output: The frequency of 'i' is: 3
 */
 
 #include <stdio.h>
 
+#define MAX 256
+
+void freqOfChar(char *str)
+{
+    int freq[MAX] = {0};
+
+    while (*str != '\0')
+    {
+        freq[(unsigned int)*str]++;
+
+        str++;
+    }
+
+    printf("characters frequency : ");
+    for (int i = 0; i < MAX; i++)
+    {
+        if (freq[i] > 0)
+        {
+            printf("'%c'=%d\n", i, freq[i]);
+        }
+    }
+}
+
 int main()
 {
+    char input[100];
 
+    printf("enter a string \n");
+    fgets(input, sizeof(input), stdin);
+
+    freqOfChar(input);
     return 0;
 }
